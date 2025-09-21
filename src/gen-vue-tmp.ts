f155c60a72b58e6bbd989a2c9fd7812904bf9a2a
Monday1997 <axu1997@qq.com>
2025-09-21 20:03:26 +0800

    feat: 处理空name异常

--- src/gen-vue-tmp.ts
+++ src/gen-vue-tmp.ts
@@ -1,30 +1,31 @@
 #!/usr/bin/env node
-import { init } from "./common-tmp";
-import { promptsOptions } from "./config/gen-vue-tmp";
-import path from "path";
-import { getDir } from "./utils/path";
+import { init } from './common-tmp'
+import { promptsOptions } from './config/gen-vue-tmp'
+import path from 'path'
+import { getDir } from './utils/path'
 
-const __dirname = getDir(import.meta.url);
-export const vueTmp = path.resolve(__dirname, "../template");
+const __dirname = getDir(import.meta.url)
+export const vueTmp = path.resolve(__dirname, '../template')
 
 type TdefaultConfig = {
-  pkgName: string;
-  config: string[];
-  css: string;
-};
+  pkgName: string
+  config: string[]
+  css: string
+}
 const defaultConfig = {
-  pkgName: "",
-  config: ["router"],
-  css: "unocss",
-};
+  pkgName: '',
+  config: ['router', 'i-18n', 'pwa', 'cdn'],
+  css: 'unocss',
+  axios: true
+}
 
 init<TdefaultConfig>({
   defaultConfig,
   tmpPath: vueTmp,
   tansformUserArgs(args: any) {
     if (args.config && !Array.isArray(args.config)) {
-      args.config = [args.config];
+      args.config = [args.config]
     }
   },
-  promptsOptions,
-});
+  promptsOptions
+})
--- src/utils/cli.ts
+++ src/utils/cli.ts
@@ -1,38 +1,38 @@
-import { isValidPackageName } from "./tools";
-import minimist from "minimist";
-import prompts from "prompts";
-import { TDealParamsWithNameArags } from "../types/cli";
-import _ from "lodash";
-import consola from "consola";
+import { isValidPackageName } from './tools'
+import minimist from 'minimist'
+import prompts from 'prompts'
+import { TDealParamsWithNameArags } from '../types/cli'
+import _ from 'lodash'
+import consola from 'consola'
 export function getUserArgs(options) {
-  return minimist(process.argv.slice(2), options);
+  return minimist(process.argv.slice(2), options)
 }
 // 需要用户带name进入时
 export async function dealParamsWithName<T extends Record<string, any>>(
   data: TDealParamsWithNameArags<T>
 ) {
-  const { defaultConfig, transformBefore, mainStep } = data;
+  const { defaultConfig, transformBefore, mainStep } = data
   const args = getUserArgs({
     alias: {
-      template: "t",
-    },
-  });
-  const [pkgName] = args._;
-  if (isValidPackageName(pkgName)) {
-    args.pkgName = pkgName;
-    transformBefore && transformBefore(args);
+      template: 't'
+    }
+  })
+  const [pkgName] = args._
+  if (pkgName && isValidPackageName(pkgName)) {
+    args.pkgName = pkgName
+    transformBefore && transformBefore(args)
     // 使用预设值 用户可以覆盖预设值
     if (args.template) {
       const result = Object.assign(
         defaultConfig,
-        _.pick(args, _.keys(defaultConfig).push("pkgName"))
-      );
-      await mainStep(result);
-      process.exit();
+        _.pick(args, _.keys(defaultConfig).push('pkgName'))
+      )
+      await mainStep(result)
+      process.exit()
     }
   } else if (pkgName || args._.length > 0) {
-    consola.error("用户输入不合法");
-    process.exit();
+    consola.error('用户输入不合法')
+    process.exit()
   }
-  prompts.override(args);
+  prompts.override(args)
 }
